9db972eaa6c1de533b9f0579cb97c7839236d9a5
Atul Dwivedi <atultechd@gmail.com>
2022-05-10 22:05:59 +0530

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,46 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
-  
-  def show
-  end
-
-  def index
-    @user = User.all
-  end
-
   def new
     @user = User.new
   end
 
+
   def edit
+    @user = User.find(params[:id])
   end
 
+
   def update
+    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to @user
+      redirect_to articles_path
+
     else
       render 'edit'
     end
   end
 
+
   def create
     @user = User.new(user_params)
+
     if @user.save
-      session[:user_id] = @user. id
-      flash[:notice] = "Welcome to the Articles blog #{@user.username}, you have successfully signed up"
+      flash[:notice] = "Welcome to the Articles Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
+
     else
       render 'new'
     end
   end
 
+
   private
 
+
   def user_params
-     params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
-
-  def set_user 
-    @user = User.find(params[:id])
-  end
-
-  def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own account"
-      redirect_to @user
-    end
-  end
+  
 end
